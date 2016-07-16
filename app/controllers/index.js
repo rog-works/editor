7d3069846a1c158163843e45d933c35d27d968d8
rog-works <rog_works@yahoo.co.jp>
2016-07-16 10:47:52 +0900

    backup

--- app/controllers/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-let router = require('express').Router();
-
-router.get('/', (request, response) => {
-	response.sendFile('/opt/app/views/index.html');
-});
-
-module.exports = router;
