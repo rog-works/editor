8b0d1122107deb41e9a4c89e1f22bca790266539
rog-works <rog_works@yahoo.co.jp>
2016-07-17 16:24:33 +0900

    fix provision

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -12,8 +12,8 @@ if [ ! -f /usr/bin/docker ]; then
 fi
 
 if [ ! -f /usr/local/bin/docker-compose ]; then
-	curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose \
-	chmod +x /usr/local/bin/docker-compose
+	curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
 fi
 
 if [ ! -f  /usr/bin/git ]; then
