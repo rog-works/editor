9ade591d743e7e11ecfd571f7fca51c52758cdac
rog-works <rog_works@yahoo.co.jp>
2016-08-06 06:18:22 +0000

    refact ace configure

--- app/_editor/app/public/js/models/editor.js
+++ app/_editor/app/public/js/models/editor.js
@@ -12,33 +12,39 @@ class Editor {
 		return self;
 	}
 
+	_configure (ext) {
+		const config = {
+			sh: {mode: 'sh', tabs: 4, softTabs: true},
+			py: {mode: 'python', tabs: 4, softTabs: true},
+			php: {mode: 'php', tabs: 4, softTabs: true},
+			css: {mode: 'css', tabs: 4, softTabs: true},
+			html: {mode: 'html', tabs: 4, softTabs: true},
+			json: {mode: 'json', tabs: 4, softTabs: true},
+			js: {mode: 'javascript', tabs: 4, softTabs: true},
+			rb: {mode: 'ruby', tabs: 2, softTabs: false},
+			yml: {mode: 'yaml', tabs: 2, softTabs: false},
+			yaml: {mode: 'yaml', tabs: 2, softTabs: false}
+		};
+		return (ext in config) ? config[ext] : config.sh;
+	}
+
 	_editor () {
 		return ace.edit('editor');
 	}
-	
-	_toMode (ext) {
-		let map = {
-			sh: 'sh',
-			py: 'python',
-			php: 'php',
-			css: 'css',
-			html: 'html',
-			json: 'json',
-			js: 'javascript',
-			rb: 'ruby',
-			yml: 'yaml',
-			yaml: 'yaml'
-		};
-		return (ext in map) ? `ace/mode/${map[ext]}` : 'ace/mode/sh';
+
+	_toMode (mode) {
+		return `ace/mode/${mode}`;
 	}
 
 	load (path = '#', content = '') {
+		const ext = path.substr(path.lastIndexOf('.') + 1);
+		const config = this._configure(ext);
 		let editor = this._editor();
 		let session = editor.getSession();
-		let ext = path.substr(path.lastIndexOf('.') + 1);
-		let mode = this._toMode(ext);
 		session.setValue(content);
-		session.setMode(mode);
+		session.setTabSize(config.tabs);
+		session.setUseSoftTabs(config.softTabs);
+		session.setMode(this._toMode(config.mode));
 		this.path(path);
 		editor.focus();
 	}
