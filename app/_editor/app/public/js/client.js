f7185c4f91835bee1bb513b5ac695a1ea3711ba7
EC2 Default User <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2016-08-18 00:03:16 +0000

    refact layout

--- app/_editor/app/public/js/client.js
+++ app/_editor/app/public/js/client.js
@@ -9,29 +9,49 @@ class Application {
 		this.entry = null;
 		this.shell = null;
 		this.log = null;
+		this.w = 0;
+		this.h = 0;
 	}
 
-	load (id = 'main') {
-		this.ws = new WS();
-		this.tool = Tool.init();
-		this.console = Console.init();
-		this.editor = Editor.init();
-		this.entry = Entry.init();
-		this.shell = Shell.init();
-		this.log = Log.init();
-		ko.applyBindings(this, document.getElementById(id));
+	load(id = 'main') {
+		try {
+			this.ws = new WS();
+			this.tool = Tool.init();
+			this.console = Console.init();
+			this.editor = Editor.init();
+			this.entry = Entry.init();
+			this.shell = Shell.init();
+			this.log = Log.init();
+			ko.applyBindings(this, document.getElementById(id));
+			this._after();
+		} catch (error) {
+			console.error(error.message, error.stack);
+		}
 	}
-};
-let APP = new Application();
-$(() => {
-	try {
-		// XXX 
-		APP.load();
-	} catch (error) {
-		console.error(error.message, error.stack);
+	
+	_after () {
+		const self = this;
+		const w = window.innerWidth;
+		const h = window.innerHeight;
+		this.w = Math.max(w, h);
+		this.h = Math.min(w, h);
+		this.resize();
+		// XXX handling for resize event
+		window.onresize = () => { self.resize(); };
 	}
-
-	const onResize = () => {
+	
+	test () {
+		try {
+			console.log('resize scale');
+			const viewport = document.getElementsByName('viewport')[0];
+			viewport.setAttribute('content', `width=device-width, initial-scale=1.0`);
+		} catch (error) {
+			console.error(error.message, error.stack);
+		}
+	}
+	
+	resize () {
+		console.log('resize');
 		// XXX Diviated with the software keyboard is Displayed
 		const w = window.innerWidth;
 		const h = window.innerHeight;
@@ -40,10 +60,12 @@ $(() => {
 		fit.height(h);
 		$('.flex-w-32').width(w - 32);
 		// XXX
-		APP.editor._editor().resize();
-	};
-	onResize();
+		this.editor._editor().resize();
+		console.log(w, h);
+		// const viewport = document.getElementsByName('viewport')[0];
+		// viewport.setAttribute('content', `width=${this.w},initial-scale=1.0`);
+	}
+}
 
-	// handling for resize event
-	$(window).on('resize', onResize);
-});
+const APP = new Application();
+window.onload = () => { APP.load(); };
