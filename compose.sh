a061d6798af52b83c6fa800daf177c2f4dfcf1fd
rog-works <rog-works@yahoo.co.jp>
2017-05-05 17:45:10 +0900

    omissions depends and chaged aws user

--- compose.sh
+++ compose.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-if [ "$(whoami)" == "ec2-user" -o "$ENV" == "aws" ]; then
+if [ "$(whoami)" == "dev" -o "$ENV" == "aws" ]; then
 	cd _compose/aws; docker-compose $*
 else
 	cd _compose/local; docker-compose $*
